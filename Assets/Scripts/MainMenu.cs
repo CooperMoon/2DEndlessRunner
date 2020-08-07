7310627463de305e39ef87637bdc19b88f6ed8c1
termi17385 <66066987+termi17385@users.noreply.github.com>
2020-08-07 16:45:29 +1000

    Test menu dont use
    THIS IS JUST A TEST

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /*Creates buttons with code 
+
+    *public void OnGUI() 
+       {
+       GUI.Box(new Rect(10, 10, 100, 90), "box");
+
+       if (GUI.Button(new Rect(20, 40, 80, 20), "BUTTON"))
+           {
+           Debug.Log("OUCH!! dont press so hard ;-;");
+
+           disable.SetActive(false); used to hide the menu when pressing options
+           count++; counts how many times button is pressed
+           }
+       else if (count > 1) if button pressed twice show menu
+           {
+           disable.SetActive(true);
+           count = 0;
+           }
+
+       if (GUI.Button(new Rect(850, 450, 100, 50), "Quit"))
+       {
+           Debug.Log("OUCH!! dont press so hard ;-;");
+
+           QuitGame();
+       }*/
+
+    public string loadScene = "GameScene";
+
+    public GameObject disable;
+    //int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+    public void StartGame()
+        {
+        SceneManager.LoadScene(loadScene);
+        }
+
+    public void QuitGame()
+        {
+        Debug.Log("Quitting Game");
+
+
+#if UNITY_EDITOR
+
+        EditorApplication.ExitPlaymode();   //exits out of playmode in unity
+
+#endif
+        
+        Application.Quit(); // < - - this will close the game application 
+
+        }
+
+
+
+    }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+    {
+    public GameObject ourGameObject;
+    public float speed;
+    public float jump;
+
+    // Start is called before the first frame update
+    void Start()
+        {
+
+        }
+
+    // Update is called once per frame
+    void Update()
+        {
+        if (Input.GetKey(KeyCode.W))
+            {
+            Vector3 gameObjectPosition = ourGameObject.transform.position;
+
+            gameObjectPosition.z = gameObjectPosition.z + (speed * Time.deltaTime);
+
+            ourGameObject.transform.position = gameObjectPosition;
+            }
+
+        if (Input.GetKey(KeyCode.S))
+            {
+            Vector3 gameObjectPosition = ourGameObject.transform.position;
+
+            gameObjectPosition.z = gameObjectPosition.z - (speed * Time.deltaTime);
+
+            ourGameObject.transform.position = gameObjectPosition;
+            }
+
+        if (Input.GetKey(KeyCode.D))
+            {
+            Vector3 gameObjectPosition = ourGameObject.transform.position;
+
+            gameObjectPosition.x = gameObjectPosition.x + (speed * Time.deltaTime);
+
+            ourGameObject.transform.position = gameObjectPosition;
+            }
+
+        if (Input.GetKey(KeyCode.A))
+            {
+            Vector3 gameObjectPosition = ourGameObject.transform.position;
+
+            gameObjectPosition.x = gameObjectPosition.x - (speed * Time.deltaTime);
+
+            ourGameObject.transform.position = gameObjectPosition;
+            }
+        if (Input.GetKeyDown("space"))
+            {
+            Vector3 gameObjectPosition = ourGameObject.transform.position;
+
+            gameObjectPosition.y = gameObjectPosition.y + jump;
+
+            ourGameObject.transform.position = gameObjectPosition;
+            }
+
+
+        }
+    }
