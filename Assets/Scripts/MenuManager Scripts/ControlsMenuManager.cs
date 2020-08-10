bd53b856cd5abbe421da2364448509c0f8294a8d
CooperMoon <cooper.moon@studytafensw.edu.au>
2020-08-10 14:57:58 +1000

    Basic MainMenu, OptionMenu & ControlsMenu

---
+++ Assets/Scripts/MenuManager Scripts/ControlsMenuManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsMenuManager : MonoBehaviour
+{
+    public void BackButton()
+    {
+        Debug.Log("ControlsMenu BackButton Pressed");
+    }
+}
---
+++ Assets/Scripts/MenuManager Scripts/OptionsMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuManager : MonoBehaviour
+{
+    public void ControlsButton()
+    {
+        Debug.Log("OptionsMenu ControlsButton Pressed");
+    }
+
+    public void BackButton()
+    {
+        Debug.Log("OptionsMenu BackButton Pressed");
+    }
+}
