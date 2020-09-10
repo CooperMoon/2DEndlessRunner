8891e18365e5ddea66f8327a86e4b237b8b4bbe7
termi17385 <66066987+termi17385@users.noreply.github.com>
2020-09-10 15:03:23 +1000

    final update
    This is the final update to the game
    code has been Commented and fixed some corruption problems hopefully

--- Assets/Scripts/Parallaxbackground.cs
+++ Assets/Scripts/Parallaxbackground.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class Parallaxbackground : MonoBehaviour
 {
-    private float length, startpos;
-    public GameObject cam;
-    public float parallaxEffect;
+    private float length, startpos;     // variable for the start and length of the sprite
+    public GameObject cam;              // variable for the camera
+    public float parallaxEffect;        // variable for how much parallax we want
 
-    // Start is called before the first frame update
+    
     void Start()
     {
+        // determines the start position
         startpos = transform.position.x;
+        // gets the length of the sprite
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
+        // used to move the sprite with the camera at the set parallax effect
         float temp = (cam.transform.position.x* (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
+        // moves the sprite
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
+        // makes sure the sprite stays with the camera and to restart the parallax
         if (temp > startpos + length) startpos += length;
         else if (temp < startpos - length) startpos -= length;
     }
